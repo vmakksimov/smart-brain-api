82d17bdf83a3a4457914b5c7436ddfacde2a8dea
vmaksimov <viktor.maksimov@oblak.bg>
2024-04-25 14:34:05 +0300

    adding post for update profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,6 +1,8 @@
+const { json } = require("body-parser");
+
 const handleProfileGet = (req, res, db) => {
   const { id } = req.params;
-  db.select('*').from('users').where({id})
+  db.select('*').from('users').where({ id })
     .then(user => {
       if (user.length) {
         res.json(user[0])
@@ -11,6 +13,29 @@ const handleProfileGet = (req, res, db) => {
     .catch(err => res.status(400).json('error getting user'))
 }
 
+
+
+const handleProfileUpdate = (req, res, db) => {
+  const { id } = req.params;
+  console.log(req.body)
+  const {name, age, pet} = req.body.formInput;
+  db('users')
+    .where({ id })
+    .update({ name })
+    .then(res => {
+      console.log("res on update", res)
+      if (res) {
+        res.json('success')
+      } else {
+        res.status(400).json('not found')
+      }
+    })
+    .catch(error => {
+      res.status(400).json('error in the catch');
+    })
+}
+
 module.exports = {
-  handleProfileGet
+  handleProfileGet,
+  handleProfileUpdate
 }
--- server.js
+++ server.js
@@ -23,17 +23,17 @@ const db = knex({
   }
   // connection: process.env.POSTGRES_URI
 });
-
+console.log('hahahha')
 const app = express();
-app.use(morgan('combined'))
-console.log('worksssfff11')
 app.use(cors())
-app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+app.use(bodyParser.json());
+app.use(morgan('combined')) // latest version of exressJS now comes with Body-Parser!
 
 app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.post('/profile/:id', (req, res) => { profile.handleProfileUpdate(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
